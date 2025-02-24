b4c965eaf0969387ddd3d0452940349432a21de5
Raphael-stefano <137648063+Raphael-stefano@users.noreply.github.com>
2025-02-24 06:17:57 -0300

    Add files via upload

---
+++ trablho-prog-web/src/main/java/com/example/trablho_prog_web/TrablhoProgWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.trablho_prog_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrablhoProgWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrablhoProgWebApplication.class, args);
+	}
+
+}
